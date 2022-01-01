aa85e97a8ba0f8a12e54f3265bc926c3c2bb1cd1
Maurice Laveaux <m.laveaux@tue.nl>
2022-01-01 22:52:41 +0100

    Moved reach to the examples directory.

---
+++ examples/reach/src/lib.rs
@@ -0,0 +1,56 @@
+extern crate ldd;
+
+mod sylvan_io;
+
+use std::error::Error;
+use std::env;
+
+/// Performs state space exploration of the given model and reports the number of states.
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>>
+{
+    // Initialize the library.
+    let mut storage = ldd::Storage::new();
+
+    let (initial_state, transitions) = sylvan_io::load_model(&mut storage, &config.filename)?;
+
+    let mut todo = initial_state.clone();
+    let mut states = initial_state.clone(); // The state space.
+
+    while todo != *storage.empty_set()
+    {
+        let todo1 = storage.empty_set().clone();
+        for transition in transitions.iter()
+        {
+            //let result = ldd::relational_product(&mut storage, &todo, &transition.relation);
+            //ldd::union(&mut storage, &todo1, &result);
+        }
+
+        //todo = ldd::minus(&mut storage, &todo1, &states);
+        states = ldd::union(&mut storage, &states, &todo);
+    }
+
+    println!("The model has {} states", ldd::len(&storage, &states));
+
+    Ok(())
+}
+
+pub struct Config
+{
+  pub filename: String,
+}
+
+impl Config
+{
+    /// Parses the provided arguments and fills in the configuration.
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str>
+    {
+        args.next(); // The first argument is the executable's location.
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Requires model filename")
+        };
+
+        Ok(Config { filename })
+    }
+}
--- examples/reach/src/main.rs
+++ examples/reach/src/main.rs
@@ -1,13 +1,11 @@
 use std::env;
 use std::process;
 
-use example::{run, Config};
+use reach::{run, Config};
 
 fn main()
 {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(
+    let config = Config::new(env::args()).unwrap_or_else(
         |err| 
         { 
             eprintln!("Problem parsing input: {}", err); 
